ae6c8e4ab4dadac13f0e47d21081e10a375cb922
DarkedgeTH <darkedgeth@gmail.com>
2022-03-07 16:34:52 -0500

    started getting Submit button to add what is typed to todo list

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -15,10 +15,24 @@ export default class App extends React.Component {
         {
           name: 'Bake Cookies',
           id: 1528817084358,
-          completed: true
+          completed: false
         }
       ]
     }
+  }
+
+  handleAdd = (name) => {
+
+    const newTodo = {
+      name: name,
+      id: Date.now(),
+      completed: false
+    }
+
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+    })
   }
 
   handleHide = () => {
@@ -39,7 +53,7 @@ export default class App extends React.Component {
 
         <TodoList todos={todos} />
 
-        <Form />
+        <Form handleAdd={this.handleAdd} />
 
         <button onClick={this.handleHide}>Hide Completed</button>
       </div>
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,13 +1,30 @@
 import React from 'react'
 
 class Form extends React.Component {
-  handleSubmit = () => {
-    console.log('submit')
+  constructor() {
+    super()
+    this.state = {
+      input: ''
+    }
   }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+    this.props.handleAdd('Take out trash')
+  }
+
+  handleChange = (evt) => {
+    this.setState({
+      ...this.state,
+      input: evt.target.value
+    })
+  }
+
   render() {
+    console.log(this.state.input)
     return (
       <form>
-        <input />
+        <input onChange={this.handleChange}/>
         <button onClick={this.handleSubmit}>Submit</button>
       </form>
     )
