0c16236173c0f7ebfb1b8e337dccc68abc37c8ab
DarkedgeTH <darkedgeth@gmail.com>
2022-03-07 15:55:15 -0500

    moved TodoList class to TodoList.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,19 +1,5 @@
 import React from 'react'
-import Todo from './Todo'
-
-class TodoList extends React.Component {
-  render(){
-    return (
-      <ul>
-        {
-          this.props.todos.map(todo => {
-            return (<Todo todo={todo} key={todo.id} />)
-          })
-        }
-      </ul>
-    )
-  }
-}
+import TodoList from './TodoList'
 
 export default class App extends React.Component {
   constructor() {
