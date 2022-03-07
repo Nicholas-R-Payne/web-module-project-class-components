a48e05ad2f2001b903f9fb3e577b362d24a93843
DarkedgeTH <darkedgeth@gmail.com>
2022-03-07 16:12:31 -0500

    added onClick event for Submit button

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 class Form extends React.Component {
+  handleSubmit = () => {
+    console.log('submit')
+  }
   render() {
     return (
       <form>
         <input />
-        <button>Submit</button>
+        <button onClick={this.handleSubmit}>Submit</button>
       </form>
     )
   }
