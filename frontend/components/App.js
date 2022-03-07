46ea2c381e4351afe948697333a5c0f276038449
DarkedgeTH <darkedgeth@gmail.com>
2022-03-07 15:20:16 -0500

    built structure of App

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,6 +5,19 @@ export default class App extends React.Component {
     return (
       <div>
         <h2>Todos:</h2>
+
+        <ul>
+          <li>Walk the dog</li>
+          <li>Learn React</li>
+          <li>Have fun</li>
+        </ul>
+
+        <form>
+          <input />
+          <button>Submit</button>
+        </form>
+        
+        <button>Hide Completed</button>
       </div>
     )
   }
